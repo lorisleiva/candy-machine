2e24862ecd9f789897d5d414a03dd17693bff9e0
Loris Leiva <loris.leiva@gmail.com>
2023-01-15 17:03:30 +0000

    wip

--- infra/build-programs.sh
+++ infra/build-programs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-(cd ./programs/candy-machine-core && cargo build)
-(cd ./programs/candy-guard && cargo build)
+(cd ./programs/candy-machine-core && cargo build-bpf)
+(cd ./programs/candy-guard && cargo build-bpf)
